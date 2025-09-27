0e6c5394e61622e9375ab2f33784b7c2feb9c92e
Alex Van der Ent <alexjvde@gmail.com>
2025-09-27 20:47:32 +0100

    Improve snake game rendering and input handling
    Optimizes rendering by drawing only changed areas for the snake and fruits, adds wall rendering, and introduces frame-based input and physics timing for smoother gameplay. Updates color configuration, refactors input logic, and removes redundant screen clearing for better performance.

--- games/snake.py
+++ games/snake.py
@@ -8,7 +8,7 @@ except:
     quit()
 
 # Config - Main
-VERSION = "0.1.0r"
+VERSION = "0.2.0r"
 
 # Config - Points
 BASIC_FRUIT_POINTS = 1
@@ -16,16 +16,18 @@ SPECIAL_FRUIT_POINTS = 5
 N_FRUITS_AT_ONCE = 3
 
 # Config - Appearance
-BACKGROUND_COLOR = (255,255,255)  # SHOULD BE INVISIBLE - Used to limit performence
+BACKGROUND_COLOR = (255,255,255)
+WALL_COLOR = (0,0,0)
 FRUIT_COLOR = (255,0,0)
 SPECIAL_FRUIT_COLOR = (0,0,255)
-SNAKE_HEAD_COLOR = (128, 255, 128)
+SNAKE_COLOR = (128, 255, 128)
 
 # Config - Gameplay
 AREA_LOCATION_X = 7
 AREA_LOCATION_Y = 4
 X2_WALL = 22
 Y2_WALL = 11
+RENDER_FRAMES_PER_PHYSICS_FRAMES = 30
 
 class Vec2:
     def __init__(self, x, y):
@@ -60,14 +62,26 @@ class Snake:
             return
         last_position = Vec2(self.positions[0].x, self.positions[0].y)
         self.positions[0].translate(v.x, v.y)
+        # Render new Head
+        x = AREA_LOCATION_X + (self.positions[0].x * 16)
+        y = AREA_LOCATION_Y + (self.positions[0].y * 16)
+        draw_rect(x,y-16,x+16,y, SNAKE_COLOR)
+        
         # Follow Body
         for (i, p) in enumerate(self.positions[1:]):
             self.positions[i+1] = Vec2(last_position.x, last_position.y)
             last_position = p
+        
         # Spawn new
         if self.to_spawn > 0:
             self.positions.append(last_position)
             self.to_spawn -= 1
+        
+        # De-Render tail
+        else:
+            x = AREA_LOCATION_X + (last_position.x * 16)
+            y = AREA_LOCATION_Y + (last_position.y * 16)
+            draw_rect(x,y-16,x+16,y, BACKGROUND_COLOR)
             
     def is_colliding_with_wall(self, x1, x2, y1, y2):
         v = self.positions[0]
@@ -88,6 +102,10 @@ class Snake:
 
 splash_screen(VERSION)
 
+# For PC
+#def getkey():
+#    return 14
+
 # Game restarts from here
 while True:
     # Main menu
@@ -95,11 +113,12 @@ while True:
     current_key = 0
     while True:
         # Up and down
-        if getkey() == 34 and getkey() != current_key: pos+=1
-        elif getkey() == 14 and getkey() != current_key: pos-=1
+        k = getkey()
+        if k == 34 and k != current_key: pos+=1
+        elif k == 14 and k != current_key: pos-=1
         if pos < 0: pos = 2
         elif pos > 2: pos = 0
-        current_key = getkey()
+        current_key = k
         
         # Select
         if getkey() == 24:
@@ -126,69 +145,81 @@ while True:
     fruits = []
     deathmethod = "none"
     score = 0
-
+    framecounter = 0
+    got_input_in_frame = False
+    
+    # Initial Render - Background
+    clear_screen()
+    if BACKGROUND_COLOR != (255,255,255): draw_rect(0,0,383,191,BACKGROUND_COLOR)
+    
+    # Initial Render - Walls
+    draw_rect(0,0,383,AREA_LOCATION_Y,WALL_COLOR)
+    draw_rect(0,AREA_LOCATION_Y+(Y2_WALL*16),383,191,WALL_COLOR)
+    draw_rect(0,0,AREA_LOCATION_X,191,WALL_COLOR)
+    draw_rect(AREA_LOCATION_X+(X2_WALL*16),0,AREA_LOCATION_X,191,WALL_COLOR)
+    show_screen()
+    
     # Ingame Loop
     while True:
-        # Clear
-        clear_screen()
-        
-        # Draw game board
-        draw_rect(AREA_LOCATION_X, AREA_LOCATION_Y, AREA_LOCATION_X + (int((X2_WALL * 16) - (len(s.positions) * 16))), AREA_LOCATION_Y + (Y2_WALL * 1), BACKGROUND_COLOR)
+        # Count them frames (There arent actually that many frames XD)
+        framecounter += 1
         
         # Get input
-        k = getkey()
-        if s.direction in ":)":
-            if k == 34: s.direction = "down"
-            elif k == 14: s.direction = "up"
-            elif k == 25: s.direction = "right"
-            elif k == 23: s.direction = "left"
-        elif s.direction in ["left", "right"]:
-            if k == 34: s.direction = "down"
-            elif k == 14: s.direction = "up"
-        else:
-            if k == 25: s.direction = "right"
-            elif k == 23: s.direction = "left"
+        if not got_input_in_frame:
+            k = getkey()
+            if s.direction in ":)":
+                if k == 34: s.direction = "down"
+                elif k == 14: s.direction = "up"
+                elif k == 25: s.direction = "right"
+                elif k == 23: s.direction = "left"
+            elif s.direction in ["left", "right"]:
+                if k == 34: s.direction = "down"
+                elif k == 14: s.direction = "up"
+            else:
+                if k == 25: s.direction = "right"
+                elif k == 23: s.direction = "left"
         
-        # Eat fruit
-        a = []
-        for f in fruits:
-            if s.is_colliding_with_fruit(f):
-                s.to_spawn += 1
-                score += f.score
-            else: a.append(f)
-        fruits = a
-        
-        # Place Fruits
-        while len(fruits) < N_FRUITS_AT_ONCE:
-            fruits.append(Fruit(Vec2(randint(1, X2_WALL-1), randint(1, Y2_WALL-1))))
-            if randint(0,5) == 0:
-                fruits.append(Fruit(Vec2(randint(1, X2_WALL-1), randint(1, Y2_WALL-1)), SPECIAL_FRUIT_POINTS, SPECIAL_FRUIT_COLOR))
-        
-        # Move snake
-        s.move()
-        
-        # Check snake collisions
-        if s.is_colliding_with_self():
-            deathmethod = "self"
-            break
+        # Decide when to process physics
+        a = RENDER_FRAMES_PER_PHYSICS_FRAMES - score
+        if a < 1: a = 1
+        if (framecounter % a) == 0:
+            # Reset input
+            got_input_in_frame = False
             
-        if s.is_colliding_with_wall(0,X2_WALL,0,Y2_WALL):
-            deathmethod = "wall"
-            break
-        
-        # Render Fruits
-        for f in fruits:
-            x = AREA_LOCATION_X + (f.position.x * 16)
-            y = AREA_LOCATION_Y + (f.position.y * 16)
-            draw_rect(x,y-16,x+16,y, f.color)
-            print(x, y)
-        
-        # Render Snake
-        for (i,p) in enumerate(s.positions):
-            x = AREA_LOCATION_X + (p.x * 16)
-            y = AREA_LOCATION_Y + (p.y * 16)
-            if i == 0: draw_rect(x,y-16,x+16,y, SNAKE_HEAD_COLOR)
-            else: draw_rect(x,y-16,x+16,y, tuple(int(a/(i*1.5)) for a in SNAKE_HEAD_COLOR))
+            # Eat fruit
+            a = []
+            for f in fruits:
+                if s.is_colliding_with_fruit(f):
+                    s.to_spawn += 1
+                    score += f.score
+                else: a.append(f)
+            fruits = a
+            
+            # Place Fruit
+            while len(fruits) < N_FRUITS_AT_ONCE:
+                if randint(0,5) == 0: fruits.append(Fruit(Vec2(randint(1, X2_WALL-1), randint(1, Y2_WALL-1)), SPECIAL_FRUIT_POINTS, SPECIAL_FRUIT_COLOR))
+                else : fruits.append(Fruit(Vec2(randint(1, X2_WALL-1), randint(1, Y2_WALL-1))))
+                # Render fruit
+                x = AREA_LOCATION_X + (fruits[-1].position.x * 16)
+                y = AREA_LOCATION_Y + (fruits[-1].position.y * 16)
+                draw_circle_solid(x+8,y-8,8,fruits[-1].color)
+                
+            # Move snake
+            s.move()
+            
+            # Check snake collisions
+            if s.is_colliding_with_self():
+                deathmethod = "self"
+                break
+                
+            if s.is_colliding_with_wall(0,X2_WALL,0,Y2_WALL):
+                deathmethod = "wall"
+                break
+            
+        # 'Dilly dally' around to increase frame time
+        if k:
+            for x in range(2):
+                draw_rect(-16,-16,0,0, WALL_COLOR)
         
         # Display
         show_screen()
@@ -211,5 +242,3 @@ while True:
             clear_screen()
             show_screen()
             break
-    
-
